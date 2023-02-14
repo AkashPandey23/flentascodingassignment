f90d0314415b0dbb772b69055557c7b8107e8f31
Akash Pandey <97802113+AkashPandey23@users.noreply.github.com>
2023-02-14 13:21:07 +0530

    Update flentasassignment.py

--- flentasassignment.py
+++ flentasassignment.py
@@ -1,8 +1,43 @@
-t = int(input())
-for _ in range(t):
-    str1 = input()
-    str2 = input()
-    if str1 in str2 or str1[::-1] in str2:
+def check_permutation_exists(text, scribbled_text):
+    scribbled_freq = [0] * 256
+    window_freq = [0] * 256
+    n = len(scribbled_text)
+    m = len(text)
+
+    # Count frequency of each character in scribbled_text
+    for i in range(n):
+        scribbled_freq[ord(scribbled_text[i])] += 1
+
+    # Initialize window
+    for i in range(n):
+        window_freq[ord(text[i])] += 1
+
+    # Check if window is a permutation of scribbled_text
+    if window_freq == scribbled_freq:
+        return True
+
+    # Slide window over text
+    for i in range(n, m):
+        window_freq[ord(text[i])] += 1
+        window_freq[ord(text[i-n])] -= 1
+
+        # Check if window is a permutation of scribbled_text
+        if window_freq == scribbled_freq:
+            return True
+
+    return False
+
+
+# Read number of test cases
+T = int(input())
+
+for i in range(T):
+    # Read pattern and text strings
+    scribbled_text = input().strip()
+    text = input().strip()
+
+    # Check if permutation of scribbled_text exists in text
+    if check_permutation_exists(text, scribbled_text):
         print("YES")
     else:
         print("NO")
